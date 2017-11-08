06ac4155237e9319ae11364b64be708d3c908c05
shaggon <shaggon.du@farfetch.com>
2017-11-08 12:30:09 +0800

    Initial Commit

---
+++ HomePage-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomePage-Demo
+//
+//  Created by farfetch on 2017/11/8.
+//  Copyright © 2017年 shaggon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomePage-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomePage-Demo
+//
+//  Created by farfetch on 2017/11/8.
+//  Copyright © 2017年 shaggon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
